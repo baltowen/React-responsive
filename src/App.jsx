37f973be6aa5a9d09fe29a8a0ced12067315eba3
Wendy Baltodano <wendybaltodano02@gmail.com>
2025-03-30 15:25:17 -0600

    Update file

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
 import './App.css';
-
+import Contact from './sections/Contact/Contact';
+import Footer from './sections/Footer/Footer';
 import Hero from './sections/Hero/Hero';
 import Projects from './sections/Projects/Projects';
 
 function App() {
   return (
- <>
+    <>
       <Hero />
       <Projects />
-      
+      <Contact />
+      <Footer />
     </>
   );
 }
